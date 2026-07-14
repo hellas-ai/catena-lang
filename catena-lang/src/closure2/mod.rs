d03c9ef45a2108d32fa2a4a7cc068ae68133db33
statusfailed <paul@statusfailed.com>
2026-07-14 15:37:31 +0100

    add closure2 module (not in lib yet)

---
+++ catena-lang/src/closure2/mod.rs
@@ -0,0 +1,10 @@
+// Closure conversion v2:
+
+// Find regions by following closure sources to targets
+mod region;
+
+// Create new definitions in the theory from each closure regions
+mod definition;
+
+// Replace closure regions with names, environments, and context operations
+mod replace;

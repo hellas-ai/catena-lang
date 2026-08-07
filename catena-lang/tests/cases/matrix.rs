33d95fa7f2eb60a7ad6f4c836cc7529498a52be6
Jani Monoses <jani.monoses@gmail.com>
2026-08-07 08:56:45 +0300

    Add more BF16 tests

--- catena-lang/tests/cases/matrix.rs
+++ catena-lang/tests/cases/matrix.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 const MATMUL_SOURCE: &str = include_str!("matrix/matmul.hex");
+const MATMUL_BF16_SOURCE: &str = include_str!("matrix/matmul-bf16.hex");
 
 #[test]
 fn f32_matmul_row_major_bufs_from_mems() -> anyhow::Result<()> {
@@ -78,5 +79,79 @@ fn f32_matmul_left_identity_view() -> anyhow::Result<()> {
     };
 
     assert_eq!(result.to_f32_vec(), input_values);
+    Ok(())
+}
+
+#[test]
+fn bf16_matmul_row_major_bufs_from_mems() -> anyhow::Result<()> {
+    use half::bf16;
+
+    let runtime = runtime_with(MATMUL_BF16_SOURCE)?;
+    let values = |values: &[f32]| {
+        values
+            .iter()
+            .copied()
+            .map(bf16::from_f32)
+            .collect::<Vec<_>>()
+    };
+    let a = runtime.mem_bf16(&values(&[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]))?;
+    let b = runtime.mem_bf16(&values(&[7.0, 8.0, 9.0, 10.0, 11.0, 12.0]))?;
+
+    let [result] = runtime.exec(
+        "matmul-2x2-via-mem",
+        [
+            a.as_ref().into(),
+            b.as_ref().into(),
+            2_u64.into(),
+            2_u64.into(),
+            3_u64.into(),
+            6_u64.into(),
+            6_u64.into(),
+            4_u64.into(),
+        ],
+    )?;
+    let Value::MemOwn(result) = result else {
+        anyhow::bail!("matmul-2x2-via-mem returned non-mem value: {result:?}");
+    };
+
+    assert_eq!(result.to_bf16_vec(), values(&[58.0, 64.0, 139.0, 154.0]));
+    Ok(())
+}
+
+#[test]
+fn bf16_matmul_right_identity_view() -> anyhow::Result<()> {
+    use half::bf16;
+
+    let runtime = runtime_with(MATMUL_BF16_SOURCE)?;
+    let input_values = [1.0_f32, 2.0, 3.0, 4.0].map(bf16::from_f32);
+    let input = runtime.mem_bf16(&input_values)?;
+    let [result] = runtime.exec(
+        "matmul-right-identity-2x2-via-mem",
+        [input.as_ref().into(), 2_u64.into(), 4_u64.into()],
+    )?;
+    let Value::MemOwn(result) = result else {
+        anyhow::bail!("matmul-right-identity-2x2-via-mem returned non-mem value: {result:?}");
+    };
+
+    assert_eq!(result.to_bf16_vec(), input_values);
+    Ok(())
+}
+
+#[test]
+fn bf16_matmul_left_identity_view() -> anyhow::Result<()> {
+    use half::bf16;
+
+    let runtime = runtime_with(MATMUL_BF16_SOURCE)?;
+    let input_values = [1.0_f32, 2.0, 3.0, 4.0].map(bf16::from_f32);
+    let input = runtime.mem_bf16(&input_values)?;
+    let [result] = runtime.exec(
+        "matmul-left-identity-2x2-via-mem",
+        [input.as_ref().into(), 2_u64.into(), 4_u64.into()],
+    )?;
+    let Value::MemOwn(result) = result else {
+        anyhow::bail!("matmul-left-identity-2x2-via-mem returned non-mem value: {result:?}");
+    };
+
+    assert_eq!(result.to_bf16_vec(), input_values);
     Ok(())
 }
